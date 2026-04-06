ed99746f0508fad7b1172d57515ab22680d64e1a
yuktadewangan2004-cyber <your-email@example.com>
2026-04-06 19:59:45 +0530

    fixed deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Use repo name as base for GitHub Pages
 export default defineConfig({
   base: '/spendor/',
   plugins: [react()],
